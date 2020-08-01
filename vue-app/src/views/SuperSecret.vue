cf6599ef44b395edceb8edb689781c77a8f06dce
Brendan C. Todahl <btodahl29@gmail.com>
2020-08-01 17:41:11 -0400

    Added api call for super secret page.

--- vue-app/src/views/SuperSecret.vue
+++ vue-app/src/views/SuperSecret.vue
@@ -1,8 +1,39 @@
 <template>
-  <div>Super Secret</div>
+  <div>
+    <div v-if="!errorMessage">
+      {{ superSecret }}
+    </div>
+    <div v-if="errorMessage">
+      {{ errorMessage }}
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      superSecret: '',
+      errorMessage: ''
+    }
+  },
+  async created() {
+    this.superSecret = await this.getSuperSecret()
+  },
+  methods: {
+    async getSuperSecret() {
+      try {
+        const response = await axios.get('api/super-secret')
+        if (response.status !== 200) throw Error(response.message)
+        return response.data
+      } catch (error) {
+        this.errorMessage = 'Unauthorized'
+      }
+    }
+  }
+}
 </script>
 
 <style>
